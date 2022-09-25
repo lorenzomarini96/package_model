2d295bffdadf887d33d14c0541d4213daed263f0
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-09-25 22:31:23 +0200

    intial import

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='package_model',
+      version=0.1,
+      description='Compute the square of a number',
+      author='Lorenzo Marini',
+      packages=['package_model'],
+      zip_safe=False)
+
